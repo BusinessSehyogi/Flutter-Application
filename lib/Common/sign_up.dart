b8ee593b036364f8766eff6886bae254286cd1db
DevTamakuwala <dev.official2424@gmail.com>
2024-11-30 21:49:20 +0530

    All work done for showing in LCA 3

--- lib/Common/sign_up.dart
+++ lib/Common/sign_up.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:business_sehyogi/Common/login.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../ipAddress.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -9,6 +15,16 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final GlobalKey<FormState> _emailFormKey = GlobalKey<FormState>();
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  bool isPasswordVisible = true;
+  bool isConfirmPasswordVisible = true;
+  bool isEmailValid = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,56 +72,225 @@ class _SignupPageState extends State<SignupPage> {
               ),
               const SizedBox(height: 40),
               // Name input field
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: "Name",
-                  labelStyle: TextStyle(color: Colors.white),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
+              // SizedBox(
+              //   width: MediaQuery.of(context).size.width * 0.9,
+              //   child: Row(
+              //     children: [
+              //       const TextField(
+              //         decoration: InputDecoration(
+              //           labelText: "Name",
+              //           labelStyle: TextStyle(color: Colors.white),
+              //           enabledBorder: UnderlineInputBorder(
+              //             borderSide: BorderSide(color: Colors.white),
+              //           ),
+              //           focusedBorder: UnderlineInputBorder(
+              //             borderSide: BorderSide(color: Colors.white),
+              //           ),
+              //         ),
+              //         style: TextStyle(color: Colors.white),
+              //       ),
+              //       // const TextField(
+              //       //   decoration: InputDecoration(
+              //       //     labelText: "Name",
+              //       //     labelStyle: TextStyle(color: Colors.white),
+              //       //     enabledBorder: UnderlineInputBorder(
+              //       //       borderSide: BorderSide(color: Colors.white),
+              //       //     ),
+              //       //     focusedBorder: UnderlineInputBorder(
+              //       //       borderSide: BorderSide(color: Colors.white),
+              //       //     ),
+              //       //   ),
+              //       //   style: TextStyle(color: Colors.white),
+              //       // ),
+              //     ],
+              //   ),
+              // ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: TextField(
+                      controller: firstNameController,
+                      decoration: const InputDecoration(
+                        labelText: "First name",
+                        labelStyle: TextStyle(color: Colors.white),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      style: const TextStyle(color: Colors.white),
+                    ),
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
+                  SizedBox(width: MediaQuery.of(context).size.width * 0.1,),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: TextField(
+                      controller: lastNameController,
+                      decoration: const InputDecoration(
+                        labelText: "Last name",
+                        labelStyle: TextStyle(color: Colors.white),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      style: const TextStyle(color: Colors.white),
+                    ),
                   ),
-                ),
-                style: TextStyle(color: Colors.white),
+                ],
               ),
               const SizedBox(height: 20),
               // Email input field
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: "E-mail",
-                  labelStyle: TextStyle(color: Colors.white),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
+            Form(
+              key: _emailFormKey,
+              child: Theme(
+                data: Theme.of(context).copyWith(
+                  inputDecorationTheme: const InputDecorationTheme(
+                    errorStyle: TextStyle(
+                      color: Colors.red,
+                      fontSize: 14,
+                    ),
                   ),
                 ),
-                style: TextStyle(color: Colors.white),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter email';
+                    }
+                    if (!isEmailValid) {
+                      return 'User already exist';
+                    }
+                    return null;
+                  },
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                  onChanged: (value) {
+                    // Reset email validity on each input change
+                    setState(() {
+                      isEmailValid = true;
+                    });
+                  },
+                ),
               ),
+            ),
               const SizedBox(height: 20),
               // Password input field
-              const TextField(
+              TextField(
+                onTap: () async {
+                  // Validate email field when password field is tapped
+                  bool emailExists = await checkEmail(emailController.text);
+                  if (emailExists) {
+                    // If email does not exist, show error
+                    setState(() {
+                      isEmailValid = false;
+                    });
+                    // Revalidate email form to show error message
+                    _emailFormKey.currentState!.validate();
+                  }
+                },
+                controller: passwordController,
                 decoration: InputDecoration(
                   labelText: "Password",
-                  labelStyle: TextStyle(color: Colors.white),
-                  enabledBorder: UnderlineInputBorder(
+                  labelStyle: const TextStyle(color: Colors.white),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      isPasswordVisible
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      _togglePasswordVisibility(context);
+                    },
+                  ),
+                  enabledBorder: const UnderlineInputBorder(
                     borderSide: BorderSide(color: Colors.white),
                   ),
-                  focusedBorder: UnderlineInputBorder(
+                  focusedBorder: const UnderlineInputBorder(
                     borderSide: BorderSide(color: Colors.white),
                   ),
                 ),
-                obscureText: true,
-                style: TextStyle(color: Colors.white),
+                obscureText: isPasswordVisible,
+                style: const TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 40),
+              TextField(
+                controller: confirmPasswordController,
+                decoration: InputDecoration(
+                  labelText: "Confirm passwords",
+                  labelStyle: const TextStyle(color: Colors.white),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      isConfirmPasswordVisible
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      _toggleConfirmPasswordVisibility(context);
+                    },
+                  ),
+                  enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+                obscureText: isConfirmPasswordVisible,
+                style: const TextStyle(color: Colors.white),
               ),
               const SizedBox(height: 40),
               // OK Button
               Align(
                 alignment: Alignment.centerRight,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    {
+                      print("API call started");
+                      var registrationURL = "http://$IP/registerUser";
+                      await http.post(Uri.parse(registrationURL),
+                          headers: {"Content-Type": "application/json"},
+                          body: jsonEncode({
+                            "userId": 0,
+                            "firstName": firstNameController.text,
+                            "lastName": lastNameController.text,
+                            "email": emailController.text,
+                            "password": passwordController.text,
+                            "contactNo": 0,
+                            "category": "Founder",
+                            "visible": true,
+                            "gender" : "U",
+                            "emailVerified" : false,
+                            "contactnoVerified" : false,
+                            "noOfIndeas": 0,
+                            "noOfConnections": 0
+                          }));
+                      print("API call finished");
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    }
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.white,
                     shape: RoundedRectangleBorder(
@@ -138,7 +323,7 @@ class _SignupPageState extends State<SignupPage> {
                     style: TextStyle(color: Colors.white, fontSize: 15),
                   ),
                   GestureDetector(
-                    onTap: () {
+                    onTap: () async {
                       Navigator.pop(context);
                       Navigator.push(
                         context,
@@ -165,4 +350,36 @@ class _SignupPageState extends State<SignupPage> {
       ),
     );
   }
+  void _togglePasswordVisibility(BuildContext context) {
+    setState(() {
+      isPasswordVisible = !isPasswordVisible;
+    });
+  }
+  void _toggleConfirmPasswordVisibility(BuildContext context) {
+    setState(() {
+      isConfirmPasswordVisible = !isConfirmPasswordVisible;
+    });
+  }
+
+  Future<bool> checkEmail(String email) async {
+    print("email check called");
+    var url = "http://$IP/checkEmail/$email";
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        var status = jsonDecode(response.body);
+        return status;
+      } else {
+        if (kDebugMode) {
+          print('Failed to load users: ${response.statusCode}');
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error: $e');
+      }
+    }
+    return false;
+  }
+
 }
